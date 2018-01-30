77942dd3f83070afc7c075aa5d7823788c7a2d2f
Nazmus Shakib <mdshuvobc@gmail.com>
2018-01-30 12:43:28 +0600

    templating.

---
+++ resources/views/layouts/footer-scripts.blade.php
@@ -0,0 +1,10 @@
+<!-- jQuery -->
+<script src="{{ asset('assets/vendors/jquery/dist/jquery.min.js') }}"></script>
+<!-- Bootstrap -->
+<script src="{{ asset('assets/vendors/bootstrap/dist/js/bootstrap.min.js') }}"></script>
+
+
+<script src="{{ asset('assets/vendorsMix/js/vendorsMix.js') }}"></script>
+
+<!-- Custom Theme Scripts -->
+<script src="{{ asset('assets/build/js/custom.min.js') }}"></script>
